1d3231042914b4c2ccc033db4acdca4bac36fffe
DESKTOP-CJ6IBEK\rise1 <rising.web.ink@gmail.com>
2023-08-30 18:21:55 +0900

    日本語化

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,21 @@ class ArticlesController < ApplicationController
     def new
       @article = Article.new
     end
+
+    def create
+      @article = Article.new(article_parms)
+      if @article.save
+        redirect_to article_path(@article), notice: 'コンテンツを保存しました'
+
+      else
+        flash.now[:error] = '保存に失敗しました'
+        render :new
+      end
+    end
+
+    private
+    def article_parms
+      params.require(:article).permit(:title, :content)
+    end
+
 end
